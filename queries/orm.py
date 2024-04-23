fc6c0c0cfa98cef30dcb4898408bcff380016276
Alexey Reukov <a.reukov@sakhalin.gov.ru>
2024-04-24 10:50:53 +1100

    Разнесена логика, добавлена обработка исключений + логирование

--- queries/orm.py
+++ queries/orm.py
@@ -15,7 +15,8 @@ from models.sqlalchemy_model import (
 )
 from models.pydantic_mun_model import (
     Article,
-    ContentBase
+    ContentBase,
+    Image
 )
 
 
@@ -40,55 +41,116 @@ class SyncOrm:
             log.debug('Справочники заполнены')
 
     @staticmethod
+    def get_article_image(article: Article) -> ImageOrm | None:
+        log.info('Получаем к статье фото')
+        if article.image:
+            try:
+                picture = ImageOrm(**article.image.model_dump())
+            except Exception as e:
+                log.exception('Не получилось данные привести к модели '
+                              f'ImageOrm\n{article.image}\n'
+                              f'Ошибка {e}')
+            log.info('Фото к статье получено')
+            return picture
+        log.info(f'Фото к статье не найдено {article.title}'
+                 f'с сайта {article.site_link}')
+        return None
+
+    @staticmethod
+    def get_content_image(
+            images: list[Image] | None = None
+            ) -> list[ImageOrm] | None:
+        log.info('Получаем контент фото')
+        if images:
+            img_list = []
+            for img in images:
+                try:
+                    img_list.append(ImageOrm(**img.model_dump()))
+                except Exception as e:
+                    log.exception('Не получилось данные привести к модели '
+                                  f'ImageOrm\n{img}\n'
+                                  f'Ошибка {e}')
+            return img_list
+        return None
+
+    @staticmethod
+    def get_article_content_block(article: Article) -> list[ContentOrm] | None:
+        log.info('Получаем контент блоки')
+        content_blocks = []
+        for content in article.content_blocks:
+            try:
+                content_for_orm = ContentBase(**content.model_dump())
+                content_orm = ContentOrm(**content_for_orm.model_dump())
+            except Exception as e:
+                log.exception(f'Произошла ошибка добавления контента {e}\n'
+                              f'{content.model_dump()}')
+            content_image = SyncOrm.get_content_image(content.images)
+            if content_image:
+                content_orm.images = content_image
+            content_blocks.append(content_orm)
+        log.info('Контент блоки полученны')
+        return content_blocks
+
+    @staticmethod
+    def get_one_or_none(query, session):
+        return session.execute(query).scalars().one_or_none()
+
+    @staticmethod
+    def get_article_tags(article: Article, session) -> list[TagOrm] | list:
+        log.info('Получаем тэги')
+        tags_list = []
+        for tag in article.tags:
+            tag_title = tag.title
+            query = (
+                select(TagOrm)
+                .filter_by(title=tag_title)
+            )
+            tag_orm = SyncOrm.get_one_or_none(query, session)
+            log.debug(f'Полученны данные {tag_orm}')
+            if not tag_orm:
+                log.info(f'Добавляем тэг {tag} в бд')
+                try:
+                    tag_orm = TagOrm(**tag.model_dump())
+                except Exception as e:
+                    log.exception('Не получилось данные привести к модели '
+                                  f'TagOrm\n{tag}\n'
+                                  f'Ошибка {e}')
+                log.info('Тэг {tag} добавлен')
+            tags_list.append(tag_orm)
+        return tags_list
+
+    @staticmethod
     def insert_news_to_db(article_clear: dict[any, any], article: Article):
         with session_fabric() as session:
             source_id = SyncOrm.get_source_id_by_url(
                 article.site_link,
                 session
                 )
+
             log.debug(f'Получение новости с сайта {article.site_link}')
             article_dict = {
                 **article_clear,
                 'source_id': source_id}
-            log.debug('Запись основной информации о статье')
-            artical_orm = ArticleOrm(**article_dict)
-            log.debug('Сохраяняем фото к статье')
-            artical_orm.image = ImageOrm(**article.image.model_dump())
-            log.debug('Начинаем работу с блоком контента')
-            content_blocks = []
-            for content in article.content_blocks:
-                try:
-                    content_for_orm = ContentBase(**content.model_dump())
-                    content_orm = ContentOrm(**content_for_orm.model_dump())
-                except Exception as e:
-                    log.error(f'{e}\n{content.model_dump()}')
-                if content.images:
-                    img_list = []
-                    for img in content.images:
-                        img_list.append(ImageOrm(**img.model_dump()))
-                    content_orm.images = img_list
-                content_blocks.append(content_orm)
-            log.debug('Сохраняем блок контента')
-            artical_orm.content_blocks = content_blocks
 
-            tags_list = []
-            for tag in article.tags:
-                tag_title = tag.title
-                query = (
-                    select(
-                        TagOrm
-                        )
-                    .filter_by(title=tag_title)
-                )
-                tag_orm = session.execute(query).scalars().one_or_none()
-                log.debug(f'Полученны данные {tag_orm}')
-                if not tag_orm:
-                    tag_orm = TagOrm(**tag.model_dump())
-                log.debug(tag_orm)
-                tags_list.append(tag_orm)
-            artical_orm.tags = tags_list
+            log.info('Попытка преобразовать данные article_dict к sql модели')
+            try:
+                article_orm = ArticleOrm(**article_dict)
+            except Exception as e:
+                log.exception('Не получилось данные привести к модели '
+                              f'ArticleOrm\n{article_dict}\n'
+                              f'Ошибка {e}')
+            log.info('Получена модель ArticleOrm')
 
-            session.add(artical_orm)
+            try:
+                article_orm.image = SyncOrm.get_article_image(article)
+                article_orm.content_blocks = (
+                    SyncOrm.get_article_content_block(article)
+                    )
+                article_orm.tags = SyncOrm.get_article_tags(article, session)
+            except Exception as e:
+                log.exception(f'{e}\n {article}')
+                raise
+            session.add(article_orm)
             session.commit()
 
     @staticmethod
